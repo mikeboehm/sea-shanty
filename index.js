cd02fd3eccfd3434c4d162c058d6ed568704a036
Mike Boehm <mike@boehm.com.au>
2019-01-27 18:57:20 +0000

    OMG ITS WORKING!

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ console.log(buttons);
 
 // //this attaches the monitoring to the underlying GPIO pin
 let fastForwardStream = phatbeat.buttonStream(29);
-// let playPauseStream = phatbeat.buttonStream(31);
-// let rewindStream = phatbeat.buttonStream(33);
-// let volUpStream = phatbeat.buttonStream(36);
-// let volDownStream = phatbeat.buttonStream(37);
-// let powerStream = phatbeat.buttonStream(32);
+let playPauseStream = phatbeat.buttonStream(31);
+let rewindStream = phatbeat.buttonStream(33);
+let volUpStream = phatbeat.buttonStream(36);
+let volDownStream = phatbeat.buttonStream(37);
+let powerStream = phatbeat.buttonStream(32);
 
 fastForwardStream.on("pinChange", function(pin, pinState){
   console.log('fastForwardStream', pin, pinState)
@@ -30,35 +30,35 @@ fastForwardStream.on("pinChange", function(pin, pinState){
   //pin state is either 1 (pressed) or 0 (released)
 });
 
-// playPauseStream.on("pinChange", function(pin, pinState){
-//   console.log('playPauseStream', pin, pinState)
-//   //pin is the pin number that has triggered the event
-//   //pin state is either 1 (pressed) or 0 (released)
-// });
+playPauseStream.on("pinChange", function(pin, pinState){
+  console.log('playPauseStream', pin, pinState)
+  //pin is the pin number that has triggered the event
+  //pin state is either 1 (pressed) or 0 (released)
+});
 
-// rewindStream.on("pinChange", function(pin, pinState){
-//   console.log('rewindStream', pin, pinState)
-//   //pin is the pin number that has triggered the event
-//   //pin state is either 1 (pressed) or 0 (released)
-// });
+rewindStream.on("pinChange", function(pin, pinState){
+  console.log('rewindStream', pin, pinState)
+  //pin is the pin number that has triggered the event
+  //pin state is either 1 (pressed) or 0 (released)
+});
 
-// volUpStream.on("pinChange", function(pin, pinState){
-//   console.log('volUpStream', pin, pinState)
-//   //pin is the pin number that has triggered the event
-//   //pin state is either 1 (pressed) or 0 (released)
-// });
+volUpStream.on("pinChange", function(pin, pinState){
+  console.log('volUpStream', pin, pinState)
+  //pin is the pin number that has triggered the event
+  //pin state is either 1 (pressed) or 0 (released)
+});
 
-// volDownStream.on("pinChange", function(pin, pinState){
-//   console.log('volDownStream', pin, pinState)
-//   //pin is the pin number that has triggered the event
-//   //pin state is either 1 (pressed) or 0 (released)
-// });
+volDownStream.on("pinChange", function(pin, pinState){
+  console.log('volDownStream', pin, pinState)
+  //pin is the pin number that has triggered the event
+  //pin state is either 1 (pressed) or 0 (released)
+});
 
-// powerStream.on("pinChange", function(pin, pinState){
-//   console.log('powerStream', pin, pinState)
-//   //pin is the pin number that has triggered the event
-//   //pin state is either 1 (pressed) or 0 (released)
-// });
+powerStream.on("pinChange", function(pin, pinState){
+  console.log('powerStream', pin, pinState)
+  //pin is the pin number that has triggered the event
+  //pin state is either 1 (pressed) or 0 (released)
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
