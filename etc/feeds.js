b512b44d83f581c0d2e613dfcbba017b8d42c3dc
Mike Boehm <mike@boehm.com.au>
2019-02-07 21:42:45 +0000

    Add feeds

---
+++ etc/feeds.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = [
+  {
+    name: 'Reply All',
+    feed: 'http://feeds.gimletmedia.com/hearreplyall'
+  },
+  {
+    name: 'The Allusionist',
+    feed: 'http://feeds.theallusionist.org/Allusionist'
+  },
+  {
+    name: '99% Invisible',
+    feed: 'http://feeds.99percentinvisible.org/99percentinvisible'
+  },
+  {
+    name: 'Radiolab',
+    feed: 'http://feeds.wnyc.org/radiolab'
+  },
+  {
+    name: 'This American Life',
+    feed: 'http://feed.thisamericanlife.org/talpodcast'
+  },
+  {
+    name: 'Planet Money',
+    feed: 'https://www.npr.org/rss/podcast.php?id=510289'
+  },
+  {
+    name: 'TED Talks Daily',
+    feed: 'http://feeds.feedburner.com/TEDTalks_audio'
+  }
+]
