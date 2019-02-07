3822c93d26a96be14d82c959c7741e3651c3948b
Mike Boehm <mike@boehm.com.au>
2019-02-07 07:46:04 +0000

    Refactor feed parser

--- tests/getLatestPodcasts.spec.js
+++ tests/getLatestPodcasts.spec.js
@@ -14,4 +14,21 @@ describe('It can get latest podcasts', () => {
 
     done()
   })
+
+  it('can gets the latest 5 episodes for each podcasts', async (done) => {
+    const feeds = [{
+      name: 'Reply All',
+      feed: 'http://feeds.gimletmedia.com/hearreplyall'
+    },
+    {
+      name: 'Radiolab',
+      feed: 'http://feeds.wnyc.org/radiolab'
+    },]
+
+    const response = await getLatestPodcasts(feeds)
+
+    expect(response.length).toBe(10)
+
+    done()
+  })
 })
